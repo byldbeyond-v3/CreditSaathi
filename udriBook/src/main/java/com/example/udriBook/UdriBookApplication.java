d0e6b282fefe3eb5ffdb6c18c18b7eef665d682f
Shrirang18 <samrougeofficial@gmail.com>
2026-07-31 21:29:11 +0530

    goot moning

--- udriBook/src/main/java/com/example/udriBook/UdriBookApplication.java
+++ udriBook/src/main/java/com/example/udriBook/UdriBookApplication.java
@@ -3,6 +3,8 @@ package com.example.udriBook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +12,14 @@ public class UdriBookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UdriBookApplication.class, args);
+	}
+
+	@Bean
+	public FlywayMigrationStrategy cleanMigrateStrategy() {
+		return flyway -> {
+			flyway.repair();
+			flyway.migrate();
+		};
 	}
 
 }
